c83fdf6a0b2f070ea68e77a87ab4606afb89b1a4
nozerx <104958959+nozerx@users.noreply.github.com>
2022-11-22 19:37:52 +0530

    basic dht connectivity implemented

--- src/p2p_network.go
+++ src/p2p_network.go
@@ -5,7 +5,6 @@ import (
 	"crypto/rand"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/libp2p/go-libp2p"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
@@ -15,6 +14,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/host/autorelay"
 	"github.com/libp2p/go-libp2p/p2p/muxer/yamux"
 	tls "github.com/libp2p/go-libp2p/p2p/security/tls"
 	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
@@ -25,7 +25,7 @@ type p2pHost struct {
 	ctx       context.Context
 	Host      host.Host
 	KaDHT     *dht.IpfsDHT
-	Discovery discovery.RoutingDiscovery
+	Discovery *discovery.RoutingDiscovery
 }
 
 func EstablishP2P() *p2pHost {
@@ -44,34 +44,50 @@ func EstablishP2P() *p2pHost {
 func creatNode(ctx context.Context) (host.Host, *dht.IpfsDHT) {
 	prvkey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, rand.Reader)
 	if err != nil {
+		fmt.Println("Error while generating the private key")
 		panic(err)
 	}
+	fmt.Println("Successfully created a private key")
 	identity := libp2p.Identity(prvkey)
 	tlstransport, err := tls.New(prvkey)
 	if err != nil {
+		fmt.Println("Error while creating a TLS transport")
 		panic(err)
 	}
+	fmt.Println("Successfully created a TLS transport")
 	security := libp2p.Security(tls.ID, tlstransport)
 	transport := libp2p.Transport(tcp.NewTCPTransport)
 	muladdr, err := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/0")
 	listen := libp2p.ListenAddrs(muladdr)
 	if err != nil {
+		fmt.Println("Error while creating the new multiaddr")
 		panic(err)
 	}
+	fmt.Println("Successfully created a multiaddrs")
 	muxer := libp2p.Muxer("/yamux/1.0.0", yamux.DefaultTransport)
-	conn := libp2p.ConnectionManager(connmgr.NewConnManager(100, 400, time.Minute))
+	connMgr, err := connmgr.NewConnManager(100, 400)
+	conn := libp2p.ConnectionManager(connMgr)
 	nat := libp2p.NATPortMap()
-	relay := libp2p.EnableAutoRelay()
+	fmt.Println("Everything good till here")
+	relay := libp2p.EnableAutoRelay(autorelay.WithDefaultStaticRelays())
+	fmt.Println("Everything works after relay enabled")
 	var kdht *dht.IpfsDHT
 	routing := libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
+		fmt.Println("Inside routing function")
 		kdht = setupKadDHT(ctx, h)
+		fmt.Println("Successfully setup kdht")
 		return kdht, err
 	})
+	fmt.Println("Routing successfull")
+	fmt.Println(relay)
 	opts := libp2p.ChainOptions(identity, listen, security, transport, muxer, conn, nat, routing, relay)
-	libhost, err := libp2p.New(ctx, opts)
+	fmt.Println("Successfully setup chain options")
+	libhost, err := libp2p.New(opts)
 	if err != nil {
+		fmt.Println("Erro while creating a new node")
 		panic(err)
 	}
+	fmt.Println("Successfully created a new node")
 	return libhost, kdht
 
 }
@@ -83,6 +99,7 @@ func setupKadDHT(ctx context.Context, nodehost host.Host) *dht.IpfsDHT {
 
 	kdht, err := dht.New(ctx, nodehost, dhtmode, dhtpeers)
 	if err != nil {
+		fmt.Println("Error while creating a new DHT")
 		panic(err)
 	}
 	return kdht
@@ -90,7 +107,8 @@ func setupKadDHT(ctx context.Context, nodehost host.Host) *dht.IpfsDHT {
 }
 
 func bootsrapDHT(ctx context.Context, nodehost host.Host, kdht *dht.IpfsDHT) {
-	if err := kdht.Bootstarp(ctx); err != nil {
+	if err := kdht.Bootstrap(ctx); err != nil {
+		fmt.Println("Error while bootstraping DHT")
 		panic(err)
 	}
 
